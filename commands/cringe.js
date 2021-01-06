fb61476467b14a6de49744650c451ca1cf6f3b74
KifoPL <michal@bemnet.pl>
2021-01-06 22:50:12 +0100

    Cosmetic changes

--- commands/cringe.js
+++ commands/cringe.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'cringe',
     description: `Express your feelings to another discord user with this beautiful poem.\nUsage: "!kifo cringe <optional_ping>"`,
     execute(message, args) {
-        let reply = "Roses are red,\nViolets are blue,\nYou are cringe\nSeriously. You're cringe.";
+        let reply = "Roses are red,\nViolets are blue,\nYou are cringe.\nSeriously. You're cringe.";
         if (args[0]) if (!isNaN(args[0])) reply = "<@" + args[0] + ">\n" + reply; else reply = args[0] + "\n" + reply;
         message.channel.send(reply);
     }
