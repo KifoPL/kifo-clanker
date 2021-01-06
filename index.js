beee26a30d864b86b455cf733938f35ced373718
KifoPL <michal@bemnet.pl>
2021-01-06 18:10:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { timeStamp } = require('console');
 const Discord = require('discord.js');
-const config = require('./config.json');
+//const config = require('./config.json');
 
 const client = new Discord.Client();
 
