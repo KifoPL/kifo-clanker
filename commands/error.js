139f35491dbe345e3282d23956e31552b4682e5c
KifoPL <michal@bemnet.pl>
2021-01-06 18:55:13 +0100

    fixed error cmd

--- commands/error.js
+++ commands/error.js
@@ -3,7 +3,7 @@ module.exports = {
     description: `If this bot encountered error anywhere, please type this command right after. It will ping me (KifoPL#3358).\nUsage: "!kifo error <optional_description>"\nWARNING! If you spam this command for no reason, you will get warned on the same premise as spam pinging. Use only when encountering actual errors.`,
     execute(message, args) {
         let reply = message.author.username + " has encountered a problem. Check it out <@289119054130839552>.";
-        if (args[0]) reply += "\nProblem:\n" + args[0];
+        if (args[0]) reply += "\nProblem:\n" + args.join(' ');
         message.channel.send(reply);
     }
 }
