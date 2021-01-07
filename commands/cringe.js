5a87e1223cd4cd961074fa5be13356ce6a814400
KifoPL <michal@bemnet.pl>
2021-01-07 03:45:58 +0100

    cringe and stfu now work better

--- commands/cringe.js
+++ commands/cringe.js
@@ -3,7 +3,24 @@ module.exports = {
     description: `Express your feelings to another discord user with this beautiful poem.\nUsage: "!kifo cringe <optional_ping>"`,
     execute(message, args) {
         let reply = "Roses are red,\nViolets are blue,\nYou are cringe.\nSeriously. You're cringe.";
-        if (args[0]) if (!isNaN(args[0])) reply = "<@" + args[0] + ">\n" + reply; else reply = args[0] + "\n" + reply;
+        if (args[0])
+        {
+            if (!isNaN(args[0]))
+            {
+                //console.log(message.guild.members.resolve(args[0]));
+                if (!message.guild.members.resolve(args[0])) return message.reply("user not found.");
+                else
+                {
+                    userid = args[0];
+                }
+            }
+            else
+            {
+                if (!message.guild.members.resolve(args[0].slice(3, args[0].length-1))) return message.reply("user not found.");
+                else userid = args[0].slice(3, args[0].length-1);
+            }
+            reply = "<@" + args[0] + ">\n" + reply;
+        }
         message.channel.send(reply);
     }
 }
