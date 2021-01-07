efc66b0c7c736f162f5a0cd0546da4dcc30d9883
KifoPL <michal@bemnet.pl>
2021-01-07 03:53:13 +0100

    fixed cringe and stfu commands
    the previous fix actually broke it more :c

--- commands/cringe.js
+++ commands/cringe.js
@@ -3,23 +3,19 @@ module.exports = {
     description: `Express your feelings to another discord user with this beautiful poem.\nUsage: "!kifo cringe <optional_ping>"`,
     execute(message, args) {
         let reply = "Roses are red,\nViolets are blue,\nYou are cringe.\nSeriously. You're cringe.";
+        if (args[1] != undefined) message.reply("Too many arguments!");
         if (args[0])
         {
-            if (!isNaN(args[0]))
+            if (message.mentions.users.firstKey() != undefined) reply = args[0] + reply;
+            else if (!isNaN(args[0]))
             {
-                //console.log(message.guild.members.resolve(args[0]));
                 if (!message.guild.members.resolve(args[0])) return message.reply("user not found.");
                 else
                 {
-                    userid = args[0];
+                    reply = "<@" + args[0] + ">\n" + reply;
                 }
             }
-            else
-            {
-                if (!message.guild.members.resolve(args[0].slice(3, args[0].length-1))) return message.reply("user not found.");
-                else userid = args[0].slice(3, args[0].length-1);
-            }
-            reply = "<@" + args[0] + ">\n" + reply;
+            
         }
         message.channel.send(reply);
     }
--- commands/stfu.js
+++ commands/stfu.js
@@ -3,23 +3,19 @@ module.exports = {
     description: `This is an eloquent way to say "Thank you for this conversation we've had".\nUsage: "!kifo stfu <optional_ping>"`,
     execute(message, args) {
         let reply = "I honestly don't even know where to start, but what you're doing is incredibly annoying, needless, and generally speaking, stupid. Continuing to do so may result in permanent damage of your brain cells. As I care for your health, please stop doing this.";
+        if (args[1] != undefined) message.reply("Too many arguments!");
         if (args[0])
         {
-            if (!isNaN(args[0]))
+            if (message.mentions.users.firstKey() != undefined) reply = args[0] + reply;
+            else if (!isNaN(args[0]))
             {
-                //console.log(message.guild.members.resolve(args[0]));
                 if (!message.guild.members.resolve(args[0])) return message.reply("user not found.");
                 else
                 {
-                    userid = args[0];
+                    reply = "<@" + args[0] + ">\n" + reply;
                 }
             }
-            else
-            {
-                if (!message.guild.members.resolve(args[0].slice(3, args[0].length-1))) return message.reply("user not found.");
-                else userid = args[0].slice(3, args[0].length-1);
-            }
-            reply = "<@" + args[0] + ">\n" + reply;
+            
         }
         message.channel.send(reply);
     }
