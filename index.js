2bf91a3c4bf8e55c060c1e55a458f74885efcb65
KifoPL <kifounaabi@gmail.com>
2021-01-10 18:37:28 +0100

    Revert "better description"
    This reverts commit 49e55ccf57f2267491b6150ee324be84f8376fb7.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ command = require(`./help.js`);
 client.commands.set(command.name, command);
 
 client.once('ready', () => {
-    console.log('Kifo Clanker™ is online!');
+    console.log('Kifo Clanker is online!');
 });
 
 client.on('message', message => {
