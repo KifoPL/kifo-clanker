c9edbaa8fd2d698bc6cbbd992bf1f3b3a1a5509e
KifoPL <kifounaabi@gmail.com>
2021-01-28 20:26:19 +0100

    2.3.0
    Added reverse command! It reverses whatever you type in.

---
+++ commands/reverse.js
@@ -0,0 +1,14 @@
+const {MessageMentions} = require('discord.js');
+module.exports = {
+    name: 'reverse',
+    description: 'This command reverses anything you type.\nUsage: "!kifo reverse <text>"',
+    execute(message, args, Discord) {
+		if (!args[0]) return message.reply("Forgot to type in text you wish to reverse. Use `!kifo reverse <text>`");
+		var array = args.join(" ").split("");
+		array.reverse();
+		var output = array.join("");
+		var unfunny = false;
+		if (output.includes("@everyone") || output.includes("@here") || output.includes("<@&")) return message.reply("if you think you're funny, think again.");
+		message.channel.send(output);
+    }
+}
