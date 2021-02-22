b9f73958d0a089995ff4d592dfda9acdf3fe519c
KifoPL <kifounaabi@gmail.com>
2021-02-22 21:01:44 +0100

    2.6.7

--- index.js
+++ index.js
@@ -139,13 +139,14 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
+    console.log("did default code work?");
     // Send the message to a designated channel on a server:
     const channel = member.guild.channels.cache.find(ch => ch.name === 'shinies-incoming');
     // Do nothing if the channel wasn't found on this server
     if (!channel) return;
     // Send the message, mentioning the member
     channel.send(`Welcome to the server, ${member}`);
-  });
+});
 
 //Code for adding WoofWoof role to members added by WoofWoofWolffe
 client.on('guildMemberAdd', member => {
