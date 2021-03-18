68568d5035d1b4befe855450ebb795d54d2679a4
KifoPL <kifounaabi@gmail.com>
2021-03-18 21:56:50 +0100

    Update superslow.js

--- commands/superslow.js
+++ commands/superslow.js
@@ -6,13 +6,13 @@ module.exports = {
     execute(message, args) {
         //This is for timestamps
         const ms = require(`ms`);
-        let shortest = ms('10s');
-        let longest = ms('14d')
+        let shortest = ms('6h');
+        let longest = ms('1y')
         if (!(message.member.permissions.has("ADMINISTRATOR"))) return message.reply("This is ADMIN ONLY command.");
         if (!args[0]) return message.reply("insufficient arguments!");
         if (isNaN(ms(args[0]))) return message.reply("incorrect time period. Please specify correct time period.");
         if (ms(args[0]) < shortest && ms(args[0]) != 0) return message.reply("Just use normal Discord slow-mode, no need to waste my bot's resources.");
-        if (ms(args[0]) > longest) return message.reply("incorrect amount of time. For the command to work, please input period of time that is between " + ms(shortest, {long : true}) + " and " + ms(shortest, {long : true}) + ".");
+        if (ms(args[0]) > longest) return message.reply("incorrect amount of time. For the command to work, please input period of time that is between " + ms(shortest, {long : true}) + " and " + ms(longest, {long : true}) + ".");
         let isOff = false;
         if (ms(args[0]) == 0) isOff = true;
         let params = [isOff, ms(args[0])];
