97f62f1b2b699f89711bbc23a729a02b1d532433
KifoPL <kifounaabi@gmail.com>
2021-03-19 21:30:19 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -20,15 +20,15 @@ module.exports = {
 			await message.guild.members.cache.filter(member => member.user.bot).each(member => {
 				if (member.user.bot) botcount++;
 			})
-			await message.guild.members.cache.filter(member => member.presence.status == "online" || member.presence.status == "dnd").each(onlinecount++);
+			await message.guild.members.cache.filter(member => member.presence.status == "online" || member.presence.status == "dnd").each(member => onlinecount++);
 			await message.guild.members.cache.filter(member => member.premiumSince != null).each(member => {
 				if (member.user.premiumSince != null) boostcount++;
 			})
-			await message.guild.roles.cache.each(rolecount++);
-			await message.guild.channels().cache.each(channelcount++);
-			await message.guild.channels().cache.filter(channel => channel.type == "voice").each(channelvoicecount++);
-			await message.guild.channels().cache.filter(channel => channel.type == "text").each(channeltextcount++);
-			await message.guild.channels().cache.filter(channel => channel.type == "category").each(channelcategorycount++);
+			await message.guild.roles.cache.each(member => rolecount++);
+			await message.guild.channels().cache.each(member => channelcount++);
+			await message.guild.channels().cache.filter(channel => channel.type == "voice").each(member => channelvoicecount++);
+			await message.guild.channels().cache.filter(channel => channel.type == "text").each(member => channeltextcount++);
+			await message.guild.channels().cache.filter(channel => channel.type == "category").each(member => channelcategorycount++);
 			let time = new Date(Date.now());
 			let servertime = time.getTime() - message.guild.createdAt.getTime();
 			newEmbed
