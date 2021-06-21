c902e999cde85e8320d430a5286cbfdc7303c65a
KifoPL <kifounaabi@gmail.com>
2021-06-21 18:39:14 +0200

    you can check pp iq and howgay in reply

--- commands/fun/pp.js
+++ commands/fun/pp.js
@@ -40,7 +40,23 @@ module.exports = {
 					userid = message.mentions.users.firstKey();
 				}
 			}
-		} else userid = message.author.id;
+		} else {
+			if (message.mentions.users.firstKey() != undefined) {
+				if (
+					!message.guild.members.resolve(
+						message.mentions.users.firstKey()
+					)
+				)
+					return message.reply(kifo.embed("user not found."));
+				if (
+					message.mentions.users.firstKey() == 289119054130839552 ||
+					message.mentions.users.firstKey() == 795638549730295820
+				)
+					Troll = true;
+				userid = message.mentions.users.firstKey();
+			}
+			userid = message.author.id;
+		}
 		if (userid == 289119054130839552 || args[0] == 795638549730295820)
 			Troll = true;
 		let username = message.guild.members.resolve(userid).displayName;
@@ -54,7 +70,7 @@ module.exports = {
 			pp = "8==============================D";
 			ppvalue = "69 cm";
 		}
-		const field = {name: pp, value: ppvalue};
+		const field = { name: pp, value: ppvalue };
 		if (isStats) return field;
 		const newEmbed = new Discord.MessageEmbed()
 			.setColor("a039a0")
